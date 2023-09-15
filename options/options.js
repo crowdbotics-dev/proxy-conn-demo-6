f174a8c9c9a60c2ac2d7bd055f47cf4d946bc239
Crowdbotics <engineering@crowdbotics.com>
2023-09-15 15:33:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proxy-conn-demo-6",
+  url: "https://proxy-conn-demo-6.botics.co",
+  api: "https://proxy-conn-demo-6.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
